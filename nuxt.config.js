a57182cd8b841ba9517ee29db6e5a2b3ff4cb41d
andresurrutia97 <andres.urrutia.997@gmail.com>
2021-10-03 17:21:03 -0500

    Added font and colors

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~/assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -35,4 +35,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  router: {
+    linkActiveClass: 'text-lh-caribean-green',
+  },
 }
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      'lh-jungle-green': '#012d26',
+      'lh-caribean-green': '#3ecfaf',
+      'lh-green-gray': '#5B6866',
+      'lh-bg-gray': '#e5f0ee',
+      white: '#fff',
+      black: '#000',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
