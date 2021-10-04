577de3db12a143b3e7242187a9819ba77f4130a1
andresurrutia97 <andres.urrutia.997@gmail.com>
2021-10-04 14:43:48 -0500

    Added add list component

--- components/EstateLists.vue
+++ components/EstateLists.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="md:grid md:gap-10 md:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4">
+  <div class="md:grid md:gap-x-10 md:gap-y-14 md:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4">
     <Card v-for="list in estateLists" :key="list.id" :list="list" />
+    <AddList />
   </div>
 </template>
 
 <script>
 import Card from '@/components/UI/Card'
+import AddList from '@/components/UI/AddList'
 
 export default {
   name: 'EstateLists',
-  components: { Card },
+  components: { Card, AddList },
   props: { estateLists: { type: Array, default: () => [] } },
 }
 </script>
